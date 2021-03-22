0d9b7daa0c27d31d90176fa920d3deecfa53d80f
Tarang97 <tarangdube8@outlook.com>
2021-03-22 20:50:42 +0530

    Created Generic Repository, Specification Pattern, DTO, AutoMapper, Custom Value resolver

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Repositories;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _ctx;
+        public GenericRepository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _ctx.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _ctx.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_ctx.Set<T>().AsQueryable(), spec);
+        }
+    }
+}
