04c532026fb33a3dd854e5ab872faf59d5aa6419
Tarang97 <tarangdube8@outlook.com>
2021-04-08 15:42:26 +0530

    Update of Models with validation and Swagger config for identity

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace API.Controllers
 
             var results = await _userManager.UpdateAsync(user);
 
-            if(results.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+            if (results.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
 
             return BadRequest("Problem updating the user");
         }
@@ -93,6 +93,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] 
+                    { "Email address is in use" } });
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
